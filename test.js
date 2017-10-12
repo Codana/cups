a5b84c9ec973692ae364bf86259ac54a73867ce0
Antti Moksi <antti.moksi@comptel.com>
2017-10-12 16:11:41 +0300

    fetcher2

--- test.js
+++ test.js
@@ -2,15 +2,14 @@
 fetch("https://restcountries.eu/rest/v2/all")
   .then(
     response => {
-        //console.log(response.status);
+      //console.log(response.status);
       response.json()
         .then(data => {
-          var d = data
-          .filter(c => c.cioc )
-          .reduce((acc, c) => {
-            return [...acc, { "short": c.cioc, "name": c.name.split(" (")[0], "region": c.region }];
-          }, []);
+          let d = data.filter(c => c.cioc && c.region == "Africa" )
+            .reduce((acc, c) => {
+              return [...acc, { "short": c.cioc, "name": c.name.split(" (")[0], "region": c.region }];
+            }, []);
           console.log(d);
-      })
+        })
     }
   );
